830c139da3b3a1659b4cf6bca5d45a99af0d740a
hujun <hujun@sksedu.com>
2020-07-26 17:22:44 +0800

    day3 作业

--- toy-react/src/ToyReact.js
+++ toy-react/src/ToyReact.js
@@ -1,10 +1,13 @@
 class ElementWrapper {
     constructor(type) {
-        this.root = document.createElement(type)
+        this.type = type;
+        this.props = Object.create(null)
+        this.children = []
     }
 
     setAttribute(name, value) {
         // \s\S  大s和小s组合在一起表示所有字符
+        /*
         if (name.match(/^on([\s\S]+)$/)) {
             console.log('ddd', RegExp.$1, value);
             let eventName = RegExp.$1.toLowerCase();
@@ -15,11 +18,14 @@ class ElementWrapper {
             name = 'class'
         }
 
-        this.root.setAttribute(name, value)
+        this.root.setAttribute(name, value)*/
+
+        this.props[name] = value
     }
 
     appendChild(vchild) {
-        let range = document.createRange();
+        this.children.push(vchild);
+        /* let range = document.createRange();
         if (this.root.children.length) {
             range.setStartAfter(this.root.lastChild)
             range.setEndAfter(this.root.lastChild);
@@ -29,25 +35,77 @@ class ElementWrapper {
         }
 
         vchild.mountTo(range);
+        */
+    }
+
+    get vdom() {
+        return this;
     }
 
     mountTo(range) {
+        this.range = range;
+
+        let placeholder = document.createComment('placeholder');
+        let endRange = document.createRange();
+        endRange.setStart(range.endContainer, range.endOffset);
+        endRange.setEnd(range.endContainer, range.endOffset);
+        endRange.insertNode(placeholder);
+
+
         range.deleteContents();
-        // range.appendChild(this.root);
-        range.insertNode(this.root)
+
+        let element = document.createElement(this.type);
+        for (let name in this.props) {
+            let value = this.props[name]
+
+            if (name.match(/^on([\s\S]+)$/)) {
+                console.log('ddd', RegExp.$1, value);
+                let eventName = RegExp.$1.toLowerCase();
+                element.addEventListener(eventName, value);
+                continue;
+            }
+    
+            if (name === 'className') {
+                name = 'class'
+            }
+
+            element.setAttribute(name, value)
+        }
+
+        for(let child of this.children) {
+            let range = document.createRange();
+            if (element.children.length) {
+                range.setStartAfter(element.lastChild)
+                range.setEndAfter(element.lastChild)
+            } else {
+                range.setStart(element, 0)
+                range.setEnd(element, 0)
+            }
+            child.mountTo(range)
+        }
+
+        range.insertNode(element)
     }
 }
 
 class TextWrapper {
     constructor(content) {
         this.root = document.createTextNode(content)
+        this.type = '#text'
+        this.children = [];
+        this.props = Object.create(null)
     }
 
     mountTo(range) {
+        this.range = range;
         // parent.appendChild(this.root)
         // 先删除在更新
         range.deleteContents()
         range.insertNode(this.root)
+    }
+
+    get vdom() {
+        return this;
     }
 }
 
@@ -55,6 +113,10 @@ export class Component {
     constructor() {
         this.props = Object.create(null);
         this.children = []
+    }
+
+    get type() {
+        return this.constructor.name;
     }
 
     setAttribute(name, value) {
@@ -74,18 +136,91 @@ export class Component {
     }
 
     update() {
-        let placeholder = document.createComment("placeholder");
-        let range = document.createRange();
-        range.setStart(this.range.endContainer, this.range.endOffset);
-        range.setEnd(this.range.endContainer, this.range.endOffset);
-        range.insertNode(placeholder);
+        // let placeholder = document.createComment("placeholder");
+        // let range = document.createRange();
+        // range.setStart(this.range.endContainer, this.range.endOffset);
+        // range.setEnd(this.range.endContainer, this.range.endOffset);
+        // range.insertNode(placeholder);
 
-        this.range.deleteContents();
+        // this.range.deleteContents();
+        
+        let vdom = this.vdom;
+        if (this.oldVdom) {
+            
+            console.log("new:", vdom)
+            console.log('old:', this.vdom)
+            // 比对两棵树。需要比对 type, props, children
+            
+            let isSameNode = (node1, node2) => {
+                if (node1.type !== node2.type) {
+                    return false;
+                }
 
-        let vdom = this.render();
-        vdom.mountTo(this.range);
+                for (let name in node1.props) {
+                    if (typeof node1.props[name] === 'object'
+                        && typeof node2.props[name] === 'object'
+                        && JSON.stringify(node1.props[name] === JSON.stringify(node2.props[name])) ){
+                        continue;
+                    }
+
+                    if (node1.props[name] !== node2.props[name]) {
+                        return false
+                    }
+                }
+
+                if (Object.keys(node1).length !== Object.keys(node2).length) {
+                    return false;
+                }
+                return true;
+            }
+
+            let isSameTree = (node1, node2) => {
+                if (!isSameNode(node1, node2)) {
+                    return false;
+                }
+                if (node1.children.length !== node2.children.length) {
+                    return false;
+                }
+
+                for(let i = 0; i < node1.children.length; i++) {
+                    if (!isSameTree(node1.children[i], node2.children[i])) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            let replace = (newTree, oldTree) => {
+                if (isSameTree(newTree, oldTree)) {
+                    console.log('all same')
+                    return;
+                }
+
+                if(!isSameNode(newTree, oldTree)) {
+                    newTree.mountTo(oldTree.range);
+                } else {
+                    for(let i = 0; i < newTree.children.length; i++) {
+                        replace(newTree.children[i], oldTree.children[i])
+                    }
+                }
+    
+                console.log('有变更哦')
+            }
+
+
+            replace(vdom, this.oldVdom)
+            
+        } else {
+            vdom.mountTo(this.range);
+        }
+
+        this.oldVdom = vdom;
 
         // placeholder.parentNode.removeChild(placeholder)
+    }
+
+    get vdom() {
+        return this.render().vdom
     }
 
     appendChild(vchild) {
@@ -95,9 +230,13 @@ export class Component {
     setState(state) {
         let merge = (oldState, newState) => {
             for(let p in newState) {
-                if (typeof newState[p] === "object") {
+                if (typeof newState[p] === "object" && newState[p] !== null) {
                     if (typeof oldState[p] !== "object")  {
-                        oldState[p] = {}
+                        if (newState[p] instanceof Array) {
+                            oldState[p] = []
+                        } else {
+                            oldState[p] = {}
+                        }
                     }
                     merge(oldState[p], newState[p])
                 } else {
@@ -112,7 +251,7 @@ export class Component {
 
         merge(this.state, state);
 
-        console.log(this.state);
+        // console.log(this.state);
         this.update();
     }
 }
@@ -136,6 +275,9 @@ export let ToyReact = {
                 if (typeof child === 'object' && child instanceof Array) {
                     insertChildren(child)
                 } else {
+                    if (child === null || child === void 0) {
+                        child = ''
+                    }
                     if(!(child instanceof Component) && !(child instanceof ElementWrapper) && !(child instanceof TextWrapper)) {
                         child = String(child)
                     }
